9f4581dc6fd182f29254b4bbed0e5cf5a61471be
lgc2333 <lgc2333@126.com>
2022-11-03 00:45:35 +0800

    update

--- scripts/update_emoji_list.py
+++ scripts/update_emoji_list.py
@@ -7,10 +7,10 @@ EMOJI_PATH = ROOT_PATH / "img" / "emoji"
 
 
 def main():
-    emo_li = [f'img/emoji/{x.name}' for x in EMOJI_PATH.iterdir() if x.is_file()]
-    with open(str(JSON_PATH), 'w', encoding='u8') as f:
+    emo_li = [f"img/emoji/{x.name}" for x in EMOJI_PATH.iterdir() if x.is_file()]
+    with open(str(JSON_PATH), "w", encoding="u8") as f:
         json.dump(emo_li, f, ensure_ascii=False, indent=2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
