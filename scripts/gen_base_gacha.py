4aeaf626f8bcf753df84120136a63632512952ce
lgc2333 <lgc2333@126.com>
2022-11-02 13:53:44 +0800

    update

--- scripts/gen_base_gacha.py
+++ scripts/gen_base_gacha.py
@@ -1,74 +1,59 @@
 import json
 from pathlib import Path
+
 import requests
 
-BASE_DICT = [
-    {
-        "server": 0,
-        "3": {"chance": 2.5, "char": []},
-        "2": {"chance": 18.5, "char": []},
-        "1": {"chance": 79.0, "char": []},
-    },
-    {
-        "server": 1,
-        "3": {"chance": 2.5, "char": []},
-        "2": {"chance": 18.5, "char": []},
-        "1": {"chance": 79.0, "char": []},
-    },
-]
+BASE_DICT = {
+    "3": {"chance": 2.5, "char": []},
+    "2": {"chance": 18.5, "char": []},
+    "1": {"chance": 79.0, "char": []},
+}
+
 PATH = Path(__file__).parent
 
 
 def main():
-    with open(str(PATH / "gacha_2star.txt"), encoding="u8") as f:
-        star2_names = f.read().split("\n")
-    with open(str(PATH / "gacha_1star.txt"), encoding="u8") as f:
-        star1_names = f.read().split("\n")
-
     stu_li = requests.get(
-        "https://lonqie.github.io/SchaleDB/data/en/students.json"
+        "https://lonqie.github.io/SchaleDB/data/cn/students.json"
     ).json()
-    res_g = []
-    res_j = []
+
+    star3 = []
     star2 = []
     star1 = []
+
     for i in stu_li:
         s_id = i["Id"]
-        match i["StarGrade"]:
-            case 3:
-                if not i["IsLimited"]:
-                    j_re, g_re = i["IsReleased"]
-                    if j_re:
-                        res_j.append(s_id)
-                    if g_re:
-                        res_g.append(s_id)
-            case 2:
-                if i["Name"].lower() in star2_names:
+        s_name = i["Name"]
+        star_grade = i["StarGrade"]
+        limited = i["IsLimited"]
+        if not limited:
+            match star_grade:
+                case 3:
+                    star3.append(s_id)
+                case 2:
                     star2.append(s_id)
-            case 1:
-                if i["Name"].lower() in star1_names:
+                case 1:
                     star1.append(s_id)
 
-    res_g.sort()
-    res_j.sort()
+        print(f'{star_grade}星{"[限定]" if limited else " 常驻 "}：({s_id}) {s_name}')
+
+    star3.sort()
     star2.sort()
     star1.sort()
 
-    BASE_DICT[0]["3"]["char"] = res_j
-    BASE_DICT[1]["3"]["char"] = res_g
-    BASE_DICT[0]["2"]["char"] = star2
-    BASE_DICT[1]["2"]["char"] = star2
-    BASE_DICT[0]["1"]["char"] = star1
-    BASE_DICT[1]["1"]["char"] = star1
-
-    print(BASE_DICT)
+    BASE_DICT["3"]["char"] = star3
+    BASE_DICT["2"]["char"] = star2
+    BASE_DICT["1"]["char"] = star1
 
     with open(str(PATH.parent / "data" / "gacha.json"), encoding="u8") as f:
         j = json.load(f)
     j["base"] = BASE_DICT
 
+    dump_j = json.dumps(j, indent=2, ensure_ascii=False)
+    print(dump_j)
+
     with open(str(PATH.parent / "data" / "gacha.json"), "w", encoding="u8") as f:
-        f.write(json.dumps(j, indent=2, ensure_ascii=False))
+        f.write(dump_j)
 
 
 if __name__ == "__main__":
--- scripts/pre-deploy.sh
+++ scripts/pre-deploy.sh
@@ -2,6 +2,6 @@
 
 SHELL_FOLDER=$(dirname "$0")
 
-pip install pathlib aiofiles aiohttp cn-sort  # requests
+pip install pathlib aiofiles aiohttp cn-sort requests
 python "${SHELL_FOLDER}/update_alias_list.py"
-# python "${SHELL_FOLDER}/gen_base_gacha.py"
+python "${SHELL_FOLDER}/gen_base_gacha.py"
