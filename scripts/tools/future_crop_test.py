6a8f4184379ec3968a36dcb31dc73b8d8bedbc84
student_2333 <lgc2333@126.com>
2023-09-08 22:27:15 +0800

    update

--- scripts/tools/future_crop_test.py
+++ scripts/tools/future_crop_test.py
@@ -8,15 +8,9 @@ if not TMP_PATH.exists():
     TMP_PATH.mkdir(parents=True)
 
 
-def main():
-    for i in TMP_PATH.iterdir():
-        if i.is_file():
-            i.unlink()
+def crop_and_save(future: dict, prefix: str):
+    print(f"cropping {prefix}")
 
-    with open(str(WIKI_JSON_PATH), encoding="u8") as f:
-        wiki_json = json.loads(f.read())
-
-    future: dict = wiki_json["global_future"]
     pic_path: str = future["img"]
     future_parts: list = future["parts"]
     banner_s, banner_e = future["banner"]
@@ -24,13 +18,25 @@ def main():
     img: Image.Image = Image.open(str(ROOT_PATH / pic_path))
 
     part_img = img.crop((0, banner_s, img.width, banner_e))
-    part_img.save(str(TMP_PATH / "banner.png"))
+    part_img.save(str(TMP_PATH / f"{prefix} _ banner.png"))
 
     for p in future_parts:
-        filename = (" _ ".join(p["date"]) + ".png").replace("/", "-")
+        date_str = " _ ".join(p["date"]).replace("/", "-")
+        filename = f"{prefix} _ {date_str}.png"
         start, end = p["part"]
         part_img = img.crop((0, start, img.width, end))
         part_img.save(str(TMP_PATH / filename))
+
+
+def main():
+    print("cleaning tmp path")
+    for i in TMP_PATH.iterdir():
+        if i.is_file():
+            i.unlink()
+
+    wiki_json = json.loads(WIKI_JSON_PATH.read_text(encoding="u8"))
+    crop_and_save(wiki_json["global_future"], "global")
+    crop_and_save(wiki_json["chinese_future"], "chinese")
 
 
 if __name__ == "__main__":
